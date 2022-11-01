f7025a84072647efd853ce2a3799972c89a6ac03
Gibbus2 <gibbusf@gmail.com>
2022-11-01 14:14:42 +0100

    removed some comments

---
+++ worldOfZuul/ForestTest.java
@@ -0,0 +1,8 @@
+package worldOfZuul;
+
+public class ForestTest{
+    public static void main(String[] args) {
+        Forest forest = new Forest();
+        System.out.println(forest.getTreePop());
+    }
+}
