312d1be87d63a4b19290eeccf823c8f1aff806f3
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2022-11-01 12:51:55 +0100

    Revert "Room Array with 16 rooms."
    This reverts commit 67fd8e66e56256955542264ac150d37256e468c3.

--- worldOfZuul/Game.java
+++ worldOfZuul/Game.java
@@ -7,43 +7,34 @@ public class Game {
     private Room currentRoom;
     private CommandWords commands;
 
-    public Room[][] rooms = new Room[4][4];
-
     public Game() {
         createRooms();
         commands = new CommandWordsImplementation();
-        currentRoom = rooms[0][0];
     }
 
     private void createRooms() {
+        Room outside, theatre, pub, lab, office;
 
-        for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms.length; j++) {
-                rooms[i][j] = new Room("tile " + i + "." + j);
-            }
-        }
+        outside = new Room("outside the main entrance of the university");
+        theatre = new Room("in a lecture theatre");
+        pub = new Room("in the campus pub");
+        lab = new Room("in a computing lab");
+        office = new Room("in the computing admin office");
 
-        for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms.length; j++) {
+        outside.setExit("east", theatre);
+        outside.setExit("south", lab);
+        outside.setExit("west", pub);
 
-                if (i > 0) {
-                    rooms[i][j].setExit("west", rooms[i-1][j]);
-                }
+        theatre.setExit("west", outside);
 
-                if (i < rooms.length - 1) {
-                    rooms[i][j].setExit("east", rooms[i+1][j]);
-                }
+        pub.setExit("east", outside);
 
-                if (j > 0) {
-                    rooms[i][j].setExit("north", rooms[i][j-1]);
-                }
+        lab.setExit("north", outside);
+        lab.setExit("east", office);
 
-                if (j < rooms.length-1) {
-                    rooms[i][j].setExit("south", rooms[i][j+1]);
-                }
-            }
-        }
+        office.setExit("west", lab);
 
+        currentRoom = outside;
     }
 
     public boolean goRoom(Command command) {
