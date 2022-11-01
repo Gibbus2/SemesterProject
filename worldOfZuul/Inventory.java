bff648f84e67a2a23a777eee702baee5627c51af
MacBech <115424555+MacBech@users.noreply.github.com>
2022-11-01 09:58:11 +0100

    Updated inventory

--- worldOfZuul/Inventory.java
+++ worldOfZuul/Inventory.java
@@ -1,4 +1,44 @@
 package worldOfZuul;
 
 public class Inventory {
+
+    private int moneyScore = 0;
+    private int sustainRating = 0;
+    private double ecoScore = 0;
+
+    public void calcMoney(int x) {
+        moneyScore += x;
+    }
+
+    public void calcEco(Room[] rooms) {
+        double ecoScoreInterim = 0;
+        for (int i = 0; i < rooms.length; i++) {
+            ecoScoreInterim += rooms[i].getForest().getTreePop();
+            //Mangler der noget for at komme ned til getTreePop()? Skal den metode flyttes til room?
+        }
+        ecoScore = ecoScoreInterim / 16;
+    }
+
+    public double calcSustain() {
+        return this.moneyScore * sustainRating;
+    }
+
+    //Skal der være parameter?
+    public int getMoneyScore() {
+        return this.moneyScore;
+    }
+
+
+    /*
+    - moneyScore : int
+    - ecoScore : double
+    - sustainRating : int
+
+    + calcMoney(int) : void
+    + calcEco(Room[]) : void
+    + calcSustain() : double
+    + getMoneyScore: int
+     */
+
+
 }
