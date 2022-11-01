ec22a07588bc8f92ba6ecae4fcf40135f6b5ab49
Gibbus2 <gibbusf@gmail.com>
2022-11-01 14:13:25 +0100

    implemented commands

--- worldOfZuul/textUI/CommandLineClient.java
+++ worldOfZuul/textUI/CommandLineClient.java
@@ -5,6 +5,8 @@
  */
 package worldOfZuul.textUI;
 
+import java.rmi.StubNotFoundException;
+
 import worldOfZuul.Command;
 import worldOfZuul.Commands;
 import worldOfZuul.Game;
@@ -42,14 +44,19 @@ public class CommandLineClient {
         System.out.println("You final Sustainability rating is based on how much money you have times how many trees are left in the beginning.");
         System.out.println("Type '" + Commands.HELP + "' if you need help.");
         System.out.println();
-        System.out.println(game.getRoomDescription());
-    }
+        this.roomInfo();
+    }   
 
     private void printHelp() {
         for(String str : game.getCommandDescriptions())
         {
             System.out.println(str + " ");
         }
+    }
+
+    private void roomInfo(){
+        System.out.println(game.getRoomDescription());
+        System.out.println("There are " + game.getCurrentRoom().getForest().getTreePop() + " trees and " + game.getCurrentRoom().getForest().getSaplingPop() + " saplings.");
     }
 
     //Controller
@@ -63,26 +70,69 @@ public class CommandLineClient {
             return false;
         }
 
-        if (commandWord == Commands.HELP) {
-            System.out.println("You are lost. You are alone. You wander");
-            System.out.println("around at the university.");
-            System.out.println();
-            System.out.println("Your command words are:");
-            printHelp();
-        } else if (commandWord == Commands.GO) {
-            if (game.goRoom(command)) {
-                System.out.println(game.getRoomDescription());
-            } else {
-                System.out.println("Can't walk in that direction.");
-            }
-        } else if (commandWord == Commands.QUIT) {
-            if (game.quit(command)) {
-                wantToQuit = true;
-            } else {
-                System.out.println("Quit what?");
-            }
+        
+        switch (commandWord) {
+            case HELP:
+                System.out.println("Your command words are:");
+                printHelp();
+                break;
+        
+            case GO:
+                if (game.goRoom(command)) {
+                    this.roomInfo();
+                } else {
+                    System.out.println("Can't walk in that direction.");
+                }
 
+                break;
+
+            case QUIT:
+                if (game.quit(command)) {
+                    wantToQuit = true;
+                } else {
+                    System.out.println("Quit what?");
+                }
+
+                break;
+
+            case PLANT:
+                System.out.println(game.getCurrentRoom().getForest().plant(Integer.parseInt(command.getCommandValue())));
+                break;
+            
+            case CHOP:
+                // String msg = command.getCommandValue();
+                // System.out.println();
+                System.out.println(game.getCurrentRoom().getForest().chop(Integer.parseInt(command.getCommandValue())));
+                break;
+            case SHOWSCORE:
+                System.out.println("You currently have " + game.getInventory().getMoneyScore() + " EuroDollars");
+                System.out.println("Current ecoScore is " + game.getInventory().calcEco(game.getRooms()));
+                break;
+            default:
+                break;
         }
+
+
+        // if (commandWord == Commands.HELP) {
+        //     System.out.println("You are lost. You are alone. You wander");
+        //     System.out.println("around at the university.");
+        //     System.out.println();
+        //     System.out.println("Your command words are:");
+        //     printHelp();
+        // } else if (commandWord == Commands.GO) {
+        //     if (game.goRoom(command)) {
+        //         System.out.println(game.getRoomDescription());
+        //     } else {
+        //         System.out.println("Can't walk in that direction.");
+        //     }
+        // } else if (commandWord == Commands.QUIT) {
+        //     if (game.quit(command)) {
+        //         wantToQuit = true;
+        //     } else {
+        //         System.out.println("Quit what?");
+        //     }
+
+        // }
         return wantToQuit;
     }
 }
