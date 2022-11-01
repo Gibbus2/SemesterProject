97f00e673d098ac38ea55b922625f9a9500b4e30
Gibbus2 <40871775+Gibbus2@users.noreply.github.com>
2022-11-01 14:43:40 +0100

    Delete ForestTest.java

--- worldOfZuul/ForestTest.java
+++
@@ -1,8 +0,0 @@
-package worldOfZuul;
-
-public class ForestTest{
-    public static void main(String[] args) {
-        Forest forest = new Forest();
-        System.out.println(forest.getTreePop());
-    }
-}
