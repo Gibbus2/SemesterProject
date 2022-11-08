ea0149459136aa5817d1d1750a8bca207f5d48c6
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2022-11-08 12:19:23 +0100

    bob removed

--- worldOfZuul/textUI/WorldOfZuulApplication.java
+++ worldOfZuul/textUI/WorldOfZuulApplication.java
@@ -16,7 +16,7 @@ public class WorldOfZuulApplication {
     public static void main(String[] args) {
         CommandLineClient client = new CommandLineClient();
         client.play();
-        System.out.println("bob");
+       
     }
 
 }
