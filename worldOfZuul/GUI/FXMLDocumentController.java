756a482246381814f1cd66d8b43647bb7860a90b
Gibbus2 <gibbusf@gmail.com>
2022-11-25 11:45:55 +0100

    added update to gridplane data

--- worldOfZuul/GUI/FXMLDocumentController.java
+++ worldOfZuul/GUI/FXMLDocumentController.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -11,23 +12,58 @@ import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
+import worldOfZuul.domain.game.Game;
+
 
 public class FXMLDocumentController implements Initializable{
     @FXML
     private GridPane map;
-    
+
+    private Game game;
+    private Label[] labels;
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // map.add(new Label("test"), 0, 0);
-        System.out.println(map.getRowCount());
+        this.game = new Game();
+    
+        labels = new Label[map.getColumnCount()*map.getRowCount()];
+        int labelIndex = 0;
+
+        for (int i = 0; i < map.getColumnCount(); i++) {
+            for (int j = 0; j < map.getRowCount(); j++){
+                labels[labelIndex] = new Label("" + i + ":" + j);
+                map.add(labels[labelIndex], i, j); 
+                labelIndex++;
+
+            }
+        }
+
         
+        this.updateMap();
     }
 
+
+    private void updateMap(){
+
+       int labelIndex = 0;
+        for (int i = 0; i < game.getRooms().length; i++) {
+            for (int j = 0; j < game.getRooms().length; j++){
+                if(game.getRooms()[j][i] == game.getCurrentRoom()){
+                    labels[labelIndex].setText("X");
+                }else{
+                    labels[labelIndex].setText(String.format("%03d", game.getRooms()[j][i].getForest().getTreePop()));
+                }
+                labelIndex++;
+            }
+        }
+    }
 
 }
