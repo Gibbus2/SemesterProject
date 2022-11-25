7682fc46e1f9a3f75d473aaa80a33996389f46fb
Gibbus2 <gibbusf@gmail.com>
2022-11-25 16:55:33 +0100

    added walking

--- worldOfZuul/GUI/FXMLDocumentController.java
+++ worldOfZuul/GUI/FXMLDocumentController.java
@@ -22,7 +22,9 @@ import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.control.Label;
-
+import worldOfZuul.domain.commands.Command;
+import worldOfZuul.domain.commands.CommandImplementation;
+import worldOfZuul.domain.commands.Commands;
 import worldOfZuul.domain.game.Game;
 
 
@@ -37,7 +39,7 @@ public class FXMLDocumentController implements Initializable{
     private Text[] tileData;
 
     @FXML
-    private Button quit;
+    private Button quit, goNorth, goEast, goSouth, goWest;
 
 
     @Override
@@ -49,22 +51,50 @@ public class FXMLDocumentController implements Initializable{
 
         for (int i = 0; i < map.getColumnCount(); i++) {
             for (int j = 0; j < map.getRowCount(); j++){
-                tileData[labelIndex] = new Text("" + i + ":" + j);
-                map.add(tileData[labelIndex], i, j); 
+                tileData[labelIndex] = new Text("" + j + ":" + i);
+                map.add(tileData[labelIndex], j, i); 
                 GridPane.setHalignment(tileData[labelIndex], HPos.CENTER);
                 GridPane.setValignment(tileData[labelIndex], VPos.CENTER);
                 labelIndex++;
             }
         }
         
-        this.updateMap();
-        this.updateInfo();
+        updateAll();
     }
 
     public void handleButtonQuit(ActionEvent event){
         WorldOfZuulApplication.quit();
     }
 
+    public void handleGoNorth(ActionEvent event){
+        goRoom("north");
+    }
+    public void handleGoEast(ActionEvent event){
+        goRoom("east");
+    }
+    public void handleGoSouth(ActionEvent event){
+        goRoom("south");
+    }
+    public void handleGoWest(ActionEvent event){
+        goRoom("west");
+    }
+
+    private void goRoom(String direction){
+        game.goRoom(game.getCommand("go", direction));
+        updateAll();
+        if(game.isGameFinished()){
+            //END
+            System.out.println("game end");
+            WorldOfZuulApplication.quit();
+        }
+    }
+
+
+    private void updateAll(){
+        updateMap();
+        updateInfo();
+        updateGoButtons();
+    }
 
     private void updateMap(){
        int labelIndex = 0;
@@ -89,6 +119,13 @@ public class FXMLDocumentController implements Initializable{
 
         this.turnsLeft.setText("" + (Game.maxTicks - game.getTick()));
         this.chopped.setText("" + game.getInventory().getWoodChopped());
+    }
+
+    private void updateGoButtons(){
+        goNorth.setDisable(game.getCurrentRoom().getExit("north") == null);
+        goEast.setDisable(game.getCurrentRoom().getExit("east") == null);
+        goSouth.setDisable(game.getCurrentRoom().getExit("south") == null);
+        goWest.setDisable(game.getCurrentRoom().getExit("west") == null);
     }
 
 }
