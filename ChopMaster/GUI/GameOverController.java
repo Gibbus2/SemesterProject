06c1013b7600e4d80da02b77661187938f447f74
MacBech <115424555+MacBech@users.noreply.github.com>
2022-12-06 14:38:46 +0100

    Sustainreview message in GameOverController

--- ChopMaster/GUI/GameOverController.java
+++ ChopMaster/GUI/GameOverController.java
@@ -14,10 +14,11 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.Initializable;
 
 public class GameOverController implements Initializable {
-    
+
     @FXML
     private TextArea textArea;
 
@@ -27,6 +28,17 @@ public class GameOverController implements Initializable {
     private Scene mainScene;
     private MainController mainController;
 
+    //Sets message at game over, based on your eco-score
+    private String sustainReview(Game game) {
+        if (game.getInventory().calcEco(game.getTiles()) >= 30000) {
+            return "Well done! Your highscore is above 30000,\nwhich proves you're a real sustainable Chop Master!";
+        }
+
+        if ((game.getInventory().calcEco(game.getTiles()) > 20000) && (game.getInventory().calcEco(game.getTiles()) < 30000)) {
+            return "You could've performed a bit better,\ntry agian - plant a few more eco friendly trees next time.";
+        }
+        return "Your eco score is too low.\nYou need to plant more trees next time \nto be a real sustainable Chop Master.";
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -34,19 +46,21 @@ public class GameOverController implements Initializable {
     }
 
     //compiles data from the game object and shows it in the testArea
-    public void showInfo(Game game){
+    public void showInfo(Game game) {
         textArea.setText(
-            "Thank you for playing ChopMaster!\nYour Final score was " + game.getInventory().calcEco(game.getTiles()) + "\nYou have chopped " + game.getInventory().getWoodChopped() +" trees\nand planted "+ game.getInventory().getTreesPlanted()+ " trees"
-        );
+                "Thank you for playing ChopMaster!\nYour Final score was " + game.getInventory().calcEco(game.getTiles()) +
+                        "\nYou have chopped " + game.getInventory().getWoodChopped() + " trees\nand planted " + game.getInventory().getTreesPlanted() + " trees" +
+                        "\n\nThe final score reflects how sustainably you have chopped.\nThe more ecologically friendly trees planted,\nthe higher the eco-score.\n"
+                        + "\nSustainable review: " + sustainReview(game));
     }
 
     //sets cancel button visability
-    public void setCancelButtonVisable(boolean value){
+    public void setCancelButtonVisable(boolean value) {
         cancel.setVisible(value);
     }
 
     //setters
-    public void setMainScene(Scene mainScene){
+    public void setMainScene(Scene mainScene) {
         this.mainScene = mainScene;
     }
 
@@ -57,13 +71,13 @@ public class GameOverController implements Initializable {
 
     //buttons
     @FXML
-    private void onQuitButtonPressed(ActionEvent event){
+    private void onQuitButtonPressed(ActionEvent event) {
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         primaryStage.close();
     }
 
     @FXML
-    private void onRestartButtonPressed(ActionEvent event){
+    private void onRestartButtonPressed(ActionEvent event) {
         mainController.start(new Game());
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         primaryStage.setScene(mainScene);
@@ -73,7 +87,7 @@ public class GameOverController implements Initializable {
     }
 
     @FXML
-    private void onCancelButtonPressed(ActionEvent event){
+    private void onCancelButtonPressed(ActionEvent event) {
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         primaryStage.setScene(mainScene);
     }
